12b475de1fa8ec28af13fe6467986e03bd8cd180
Niklas Sauter <niki02.sauter@gmx.de>
2019-07-04 16:43:51 +0200

    remove floating_action_button_child.dart

--- lib/floating_action_row.dart
+++ lib/floating_action_row.dart
@@ -1,6 +1,5 @@
 library floating_action_row;
 
 export 'src/floating_action_row.dart';
-export 'src/floating_action_row_child.dart';
 export 'src/floating_action_row_button.dart';
 export 'src/floating_action_row_divider.dart';
